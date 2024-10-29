625446742fe3c76dd3b4f86ab348bf1154bc25da
ElBi21 <leonardo.biason@gmail.com>
2024-10-29 11:02:41 +0100

    Finished 03-mat-mul.c

--- 3rd Year/Programmazione di Sistemi Embedded e Multicore/code/02 - MPI/Exercises/03-mat-mul.c
+++ 3rd Year/Programmazione di Sistemi Embedded e Multicore/code/02 - MPI/Exercises/03-mat-mul.c
@@ -5,7 +5,7 @@
 #include <stdbool.h>
 
 int rand_int() {
-    return rand() / 1000000;
+    return rand() / 20000000;
 }
 
 int* new_mat(int rows, int cols, bool transpose, int rank) {
@@ -16,7 +16,7 @@ int* new_mat(int rows, int cols, bool transpose, int rank) {
         rows = cols;
         cols = temp_rows;
     }
-    printf("[INFO] Rank %d is creating a matrix of shape %d × %d\n", rank, rows, cols);
+    // printf("[INFO] Rank %d is creating a matrix of shape %d × %d\n", rank, rows, cols);
     matrix = (int*) malloc(rows * cols * sizeof(int));
     return matrix;
 }
@@ -36,24 +36,21 @@ int main(int argc, char** argv) {
     int cols_1 = atoi(argv[2]);
     int rows_2 = atoi(argv[3]);
     int cols_2 = atoi(argv[4]);
+
+    if (cols_1 != rows_2) {
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
     
     int n_1 = rows_1 * cols_1;
     int n_2 = rows_2 * cols_2;
     
     int* mat_1 = new_mat(rows_1, cols_1, false, rank);
-    int* mat_2 = new_mat(rows_2, cols_2, true, rank);
+    int* mat_2 = new_mat(rows_2, cols_2, false, rank);
     int* mat_1_partial = new_mat((rows_1 / size), cols_1, false, rank);
+    int* res_partial = new_mat((rows_1 / size), cols_2, false, rank);
 
-    // If rank is not p-1, then create a partial matrix
-    /*if (rank != (size - 1)) {
-        free(mat_1);
-    } else {
-        free(mat_1_partial);
-    }*/
-
-
+    // Fill the matrices
     if (rank == (size - 1)) {
-        // Fill the matrices
         for (int i = 0; i < n_1; i++) {
             mat_1[i] = rand_int();
         }
@@ -66,10 +63,50 @@ int main(int argc, char** argv) {
     int partial_n = (rows_1 / size) * cols_1;
 
     MPI_Bcast(mat_2, n_2, MPI_INT, scatter_root, MPI_COMM_WORLD);
-    MPI_Scatter(mat_1, partial_n, MPI_INT,
-        mat_1_partial, partial_n, MPI_INT, (size - 1), MPI_COMM_WORLD);
-    
-    // Make multiplication    
+
+    if (rank == (size - 1)) {
+        MPI_Scatter(mat_1, partial_n, MPI_INT,
+            MPI_IN_PLACE, partial_n, MPI_INT, (size - 1), MPI_COMM_WORLD);
+
+        for (int r = 0; r < (rows_1 / size); r++) {     // For each row of mat_1
+            for (int c = 0; c < cols_2; c++) {          // For each col of mat_2
+                int val = 0;
+                for (int e = 0; e < cols_1; e++) {      // For each element of the column of mat_1
+                    val = val + mat_1[(r * cols_1) + e] + mat_2[(c * cols_1) + e];
+                }
+                res_partial[(r * cols_2) + c] = val;
+            }
+        }
+
+    } else {
+        MPI_Scatter(mat_1, partial_n, MPI_INT,
+            mat_1_partial, partial_n, MPI_INT, (size - 1), MPI_COMM_WORLD);
+
+        for (int r = 0; r < (rows_1 / size); r++) {     // For each row of the temp mat_1
+            for (int c = 0; c < cols_2; c++) {          // For each col of mat_2
+                int val = 0;
+                for (int e = 0; e < cols_1; e++) {      // For each element of the column of temp mat_1
+                    val = val + mat_1_partial[(r * cols_1) + e] + mat_2[(c * cols_1) + e];
+                }
+                res_partial[(r * cols_2) + c] = val;
+            }
+        }
+    }
+
+    int* res = new_mat(rows_1, cols_2, false, 0);
+    MPI_Gather(res_partial, (rows_1 / size) * cols_2, MPI_INT, 
+        res, (rows_1 / size) * cols_2, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Test to see if the matrix is full or not
+    if (rank == 0) {
+        for (int r = 0; r < 12; r++) {
+            printf("Result[%d] ", r + 1);
+            for (int i = 0; i < 11; i++) {
+                printf("%d ", res[(r * cols_2) + i]);
+            }
+            printf("\n");
+        }
+    }
 
     MPI_Finalize();
     return 0;
